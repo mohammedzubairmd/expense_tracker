be4437c86b377fa3fa72995f295725dc985ed573
Mohammed Zubair <mohammedzubair4940@gmail.com>
2026-02-05 17:45:11 +0530

    Update expense_tracker.sql

--- expense_tracker.sql
+++ expense_tracker.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE expense_tracker;
-USE expense_tracker;
-DROP TABLE expenses;
+CREATE DATABASE IF NOT EXISTS expense_tracker;
+
 USE expense_tracker;
 
-CREATE TABLE expenses (
+CREATE TABLE IF NOT EXISTS expenses (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    amount DOUBLE,
-    category VARCHAR(255),
-    date DATE,
+    title VARCHAR(255) NOT NULL,
+    amount DOUBLE NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    date DATE NOT NULL,
     type VARCHAR(255)
 );
